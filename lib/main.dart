55c44c3145b4779b8196d71bab3d737f20f9e897
Jules <boismond.jules@gmail.com>
2024-12-12 18:43:53 +0100

    app en portrait uniquement

--- lib/main.dart
+++ lib/main.dart
@@ -7,25 +7,29 @@ import 'package:provider/provider.dart';
 import 'controllers/providers/UserProvider.dart';
 import 'package:ecoward/theme/light_mode.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => UserProvider(),
-        ),
-        ChangeNotifierProvider(
-          create: (_) => Pageprovider(),
-        ),
-        ChangeNotifierProvider(
-          create: (_) => ActionProvider(),
-        ),
-      ],
-      child: MyApp(),
-    ),
-  );
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
+    runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (_) => UserProvider(),
+          ),
+          ChangeNotifierProvider(
+            create: (_) => Pageprovider(),
+          ),
+          ChangeNotifierProvider(
+            create: (_) => ActionProvider(),
+          ),
+        ],
+        child: MyApp(),
+      ),
+    );
+  });
 }
 
 class MyApp extends StatelessWidget {
